6027312f0d8dad5d47b07f018bab8bc926b5dbc5
Aeralyn <Aeralyn123@gmail.com>
2025-04-23 19:53:48 -0600

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./api/userRoutes");
 const thoughtRoutes = require("./api/thoughtRoutes");
 
-// Prefix all API routes with /api
-router.use("/api/users", userRoutes);
-router.use("/api/thoughts", thoughtRoutes);
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
